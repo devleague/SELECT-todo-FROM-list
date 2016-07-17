b3894ff105d3b435758474e1914647193f9e91c8
Lisa <misslisazhou@gmail.com>
2016-07-16 18:19:33 -1000

    complete statements for 1-7

---
+++ todo_app.sql
@@ -0,0 +1,21 @@
+-- Write a query to drop a database named todo_app if it exists
+DROP DATABASE IF EXISTS todo_app;
+-- Write a query to create a database named todo_app
+CREATE DATABASE todo_app;
+-- Write a query to delete a user named michael if it exists
+DROP USER IF EXISTS michael;
+-- Write a query to create a user named michael with an encrypted password stonebreaker
+CREATE USER michael
+  WITH
+    ENCRYPTED PASSWORD 'stonebreaker';
+-- Connect to the newly created database
+\c todo_app;
+-- Create a table named tasks
+CREATE TABLE tasks (
+  id SERIAL NOT NULL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+  updated_at TIMESTAMP WITHOUT TIME ZONE,
+  completed BOOLEAN DEFAULT false
+  );

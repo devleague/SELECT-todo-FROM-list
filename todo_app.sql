473a77bb94adf5ad57822cb30e10e63d68209ad9
Christie R <creindle@hawaii.edu>
2016-07-16 19:47:23 -1000

    This is taking so long :O

--- todo_app.sql
+++ todo_app.sql
@@ -1,7 +1,62 @@
 DROP DATABASE IF EXISTS todo_app;
 CREATE DATABASE todo_app;
 
+\c todo_app;
+
 DROP USER IF EXISTS michael;
-CREATE USER michael 
-    WITH 
+CREATE USER michael
+    WITH
         ENCRYPTED PASSWORD 'stonebreaker';
+
+CREATE TABLE tasks (
+  id          SERIAL PRIMARY KEY,
+  title       varchar(255),
+  description TEXT              NULL,
+  created_at  TIMESTAMP         DEFAULT now(),
+  updated_at  TIMESTAMP         NULL,
+  completed   BOOLEAN           DEFAULT FALSE
+);
+
+INSERT INTO tasks (
+  id,
+  title,
+  description,
+  created_at,
+  updated_at,
+  completed
+  )
+
+  VALUES (
+    DEFAULT,
+    'Study SQL',
+    'Complete this exercise',
+    DEFAULT,
+    NULL,
+    NULL
+  );
+
+SELECT * FROM tasks;
+
+ALTER TABLE tasks
+  DROP COLUMN completed;
+
+ALTER TABLE tasks
+  ADD COLUMN completed_at TIMESTAMP DEFAULT NULL;
+
+UPDATE tasks
+  SET updated_at=now()
+  WHERE updated_at IS NULL;
+
+ALTER TABLE tasks
+  ALTER COLUMN updated_at SET NOT NULL;
+
+ALTER TABLE tasks
+  ALTER COLUMN updated_at SET DEFAULT now();
+
+
+
+SELECT * FROM tasks;
+
+
+
+
